8dd76af042adb6774ec6287adc78f8ff149dc976
Pete Peterson <petersonpf@ornl.gov>
2014-04-22 01:59:50 -0400

    Adding crappy upgrade to social links.

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -1,9 +1,10 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
-include Nanoc3::Helpers::LinkTo
-include Nanoc3::Helpers::XMLSitemap
 include Nanoc3::Helpers::Breadcrumbs
-include Nanoc3::Helpers::Tagging
+include Nanoc3::Helpers::HTMLEscape
+include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Rendering
+include Nanoc3::Helpers::Tagging
+include Nanoc3::Helpers::XMLSitemap
 include RandomTextHelper
 include WebApiHelper
