22d08d782a31c56b016eb77454d3a9d236d7fb2d
Pete Peterson <petersonpf@ornl.gov>
2014-04-22 02:16:41 -0400

    Getting rid of html_escape helper

--- lib/helpers_.rb
+++ lib/helpers_.rb
@@ -1,7 +1,6 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
 include Nanoc3::Helpers::Breadcrumbs
-include Nanoc3::Helpers::HTMLEscape
 include Nanoc3::Helpers::LinkTo
 include Nanoc3::Helpers::Rendering
 include Nanoc3::Helpers::Tagging
