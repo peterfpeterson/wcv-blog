483133e15476da19181aa0a4815c02bf6a09b9cf
Pete Peterson <petersonpf@ornl.gov>
2014-07-15 00:55:05 -0400

    Re #4. Changed javascript includes.

--- lib/helpers/web_api.rb
+++ lib/helpers/web_api.rb
@@ -7,10 +7,18 @@ module WebApiHelper
       result << "https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"
     elsif name == "d3"
       result << "http://d3js.org/d3.v3.min.js"
+    elsif name == "dynatable"
+      result << "/js/vendor/dynatable-0.3.1.js"
+    elsif name == "jquery"
+        result << "//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js\"></script>\n  "
+        result << "<script>window.jQuery || document.write('<script src=\"/js/vendor/jquery-1.11.1.min.js\"><\\/script>')</script>\n  "
+        result << "<script src=\"js/plugins.js\"></script>\n  "
+        result << "<script src=\"js/main.js"
     else
       # just use the supplied name as is
       result << name
     end
+
     result << "\"></script>"
 
     result # echo the result
